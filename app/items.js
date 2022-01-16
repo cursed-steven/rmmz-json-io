23fc4b0c721f00a98426e3a12ff89b24aecc1532
cursed-steven <imaoka.hiroki@gmail.com>
2022-01-16 16:06:01 +0900

    列順変更、説明追記

--- app/items.js
+++ app/items.js
@@ -70,54 +70,54 @@ function convertItemsJsonToCsv() {
     let col;
     const col0 = [
         'ID', 
-        'アニメーションID', 
-        '消耗', 
-        '会心', 
-        '属性ID', 
-        '計算式', 
-        'ダメージタイプ', 
-        '分散度', 
-        '説明', 
-        '使用効果(編集不可)', 
-        '命中タイプ', 
-        'アイコンインデックス', 
-        'アイテムタイプ', 
         '名前', 
-        'メモ', 
-        '使用可能時', 
+        'アイコン', 
+        '説明', 
+        'アイテムタイプ', 
         '価格', 
-        '連続回数', 
+        '消耗', 
         '範囲', 
+        '使用可能時', 
         '速度補正', 
         '成功率', 
+        '連続回数', 
         '得TP', 
+        '命中タイプ', 
+        'アニメーションID', 
+        'ダメージタイプ', 
+        '属性ID', 
+        '計算式', 
+        '分散度', 
+        '会心', 
+        '使用効果(編集不可)', 
+        'メモ', 
     ];
     rows.push(col0.join("\t"));
 
     for (let i = 1; i < $objJson.length; i++) {
         col = [
             $objJson[i].id, 
-            $objJson[i].animationId, 
-            $objJson[i].consumable, 
-            $objJson[i].damage.critical, 
-            $objJson[i].damage.elementId, 
-            $objJson[i].damage.formula, 
-            $objJson[i].damage.type, 
-            $objJson[i].damage.variance, 
-            $objJson[i].description.replaceAll("\n", "<改行>"), 
-            encodeJsonData($objJson[i].effects), 
-            $objJson[i].hitType, 
-            $objJson[i].iconIndex, 
-            $objJson[i].itypeId, 
             $objJson[i].name, 
-            $objJson[i].note.replaceAll("\n", "<改行>"), 
-            $objJson[i].occasion, 
+            $objJson[i].iconIndex, 
+            $objJson[i].description.replaceAll("\n", "<改行>"), 
+            $objJson[i].itypeId, 
             $objJson[i].price, 
-            $objJson[i].repeats, 
+            $objJson[i].consumable, 
             $objJson[i].scope, 
+            $objJson[i].occasion, 
             $objJson[i].speed, 
             $objJson[i].successRate, 
+            $objJson[i].repeats, 
             $objJson[i].tpGain, 
+            $objJson[i].hitType, 
+            $objJson[i].animationId, 
+            $objJson[i].damage.type, 
+            $objJson[i].damage.elementId, 
+            $objJson[i].damage.formula, 
+            $objJson[i].damage.variance, 
+            $objJson[i].damage.critical, 
+            encodeJsonData($objJson[i].effects), 
+            $objJson[i].note.replaceAll("\n", "<改行>"), 
         ];
         rows.push(col.join("\t"));
     }
@@ -136,29 +136,29 @@ function convertItemsCsvToJson() {
 
         item = newItem();
         item.id = parseInt(col[0]);
-        item.animationId = parseInt(col[1]);
-        item.consumable = col[2] == 'true' || col[2] == '1' ? true : false;
+        item.name = col[1];
+        item.iconIndex = parseInt(col[2]);
+        item.description = col[3];
+        item.itypeId = parseInt(col[4]);
+        item.price = parseInt(col[5]);
+        item.consumable = col[6] == 'true' || col[6] == '1' ? true : false;
+        item.scope = parseInt(col[7]);
+        item.occasion = parseInt(col[8]);
+        item.speed = parseInt(col[9]);
+        item.successRate = parseInt(col[10]);
+        item.repeats = parseInt(col[11]);
+        item.tpGain = parseInt(col[12]);
+        item.hitType = parseInt(col[13]);
+        item.animationId = parseInt(col[14]);
         item.damage = {
-            critical: col[3] == 'true' || col[3] == '1' ? true : false, 
-            elementId: parseInt(col[4]), 
-            formula: col[5], 
-            type: parseInt(col[6]), 
-            variance: parseInt(col[7]), 
+            type: parseInt(col[15]), 
+            elementId: parseInt(col[16]), 
+            formula: col[17], 
+            variance: parseInt(col[18]), 
+            critical: col[19] == 'true' || col[19] == '1' ? true : false, 
         };
-        item.description = col[8];
-        item.effects = decodeJsonData(col[9]);
-        item.hitType = parseInt(col[10]);
-        item.iconIndex = parseInt(col[11]);
-        item.itypeId = parseInt(col[12]);
-        item.name = col[13];
-        item.note = col[14];
-        item.occasion = parseInt(col[15]);
-        item.price = parseInt(col[16]);
-        item.repeats = parseInt(col[17]);
-        item.scope = parseInt(col[18]);
-        item.speed = parseInt(col[19]);
-        item.successRate = parseInt(col[20]);
-        item.tpGain = parseInt(col[21]);
+        item.effects = decodeJsonData(col[20]);
+        item.note = col[21];
 
         json.push(item);
     }
--- app/skills.js
+++ app/skills.js
@@ -70,63 +70,63 @@ function convertSkillsJsonToCsv() {
     let col;
     const col0 = [
         'ID', 
-        'アニメーションID', 
-        '会心', 
-        '属性', 
-        '計算式', 
-        'ダメージタイプ', 
-        '分散度', 
+        '名前', 
+        'アイコン', 
         '説明', 
-        '効果(編集不可)', 
+        'スキルタイプ', 
+        '消費MP', 
+        '消費TP', 
+        '範囲', 
+        '使用可能時', 
+        '速度補正', 
+        '成功率', 
+        '連続回数', 
+        '得TP', 
         '命中タイプ', 
-        'アイコンインデックス', 
+        'アニメーションID', 
         'メッセージ1行目', 
         'メッセージ2行目', 
-        '消費MP', 
-        '名前', 
-        'メモ', 
-        '使用可能時', 
-        '連続回数', 
         '必要武器タイプ1', 
         '必要武器タイプ2', 
-        '範囲', 
-        '速度補正', 
-        'スキルタイプID', 
-        '成功率', 
-        '消費TP', 
-        '得TP', 
-        'メッセージタイプ', 
+        'ダメージタイプ', 
+        '属性', 
+        '計算式', 
+        '分散度', 
+        '会心', 
+        '使用効果(編集不可)', 
+        'メモ', 
+        '???', 
     ];
     rows.push(col0.join("\t"));
 
     for (let i = 1; i < $objJson.length; i++) {
         col = [
             $objJson[i].id, 
-            $objJson[i].animationId, 
-            $objJson[i].damage.critical, 
-            $objJson[i].damage.elementId, 
-            $objJson[i].damage.formula, 
-            $objJson[i].damage.type, 
-            $objJson[i].damage.variance, 
-            $objJson[i].description.replaceAll("\n", "<改行>"), 
-            encodeJsonData($objJson[i].effects), 
-            $objJson[i].hitType, 
+            $objJson[i].name, 
             $objJson[i].iconIndex, 
+            $objJson[i].description.replaceAll("\n", "<改行>"), 
+            $objJson[i].stypeId, 
+            $objJson[i].mpCost, 
+            $objJson[i].tpCost, 
+            $objJson[i].scope, 
+            $objJson[i].occasion, 
+            $objJson[i].speed, 
+            $objJson[i].successRate, 
+            $objJson[i].repeats, 
+            $objJson[i].tpGain, 
+            $objJson[i].hitType, 
+            $objJson[i].animationId, 
             $objJson[i].message1, 
             $objJson[i].message2, 
-            $objJson[i].mpCost, 
-            $objJson[i].name, 
-            $objJson[i].note.replaceAll("\n", "<改行>"), 
-            $objJson[i].occasion, 
-            $objJson[i].repeats, 
             $objJson[i].requiredWtypeId1, 
             $objJson[i].requiredWtypeId2, 
-            $objJson[i].scope, 
-            $objJson[i].speed, 
-            $objJson[i].stypeId, 
-            $objJson[i].successRate, 
-            $objJson[i].tpCost, 
-            $objJson[i].tpGain, 
+            $objJson[i].damage.type, 
+            $objJson[i].damage.elementId, 
+            $objJson[i].damage.formula, 
+            $objJson[i].damage.variance, 
+            $objJson[i].damage.critical, 
+            encodeJsonData($objJson[i].effects), 
+            $objJson[i].note.replaceAll("\n", "<改行>"), 
             $objJson[i].messageType, 
         ];
         rows.push(col.join("\t"));
@@ -146,33 +146,33 @@ function convertSkillsCsvToJson() {
 
         skill = newSkill();
         skill.id = parseInt(col[0]);
-        skill.animationId = parseInt(col[1]);
+        skill.name = col[1];
+        skill.iconIndex = parseInt(col[2]);
+        skill.description = col[3];
+        skill.stypeId = parseInt(col[4]);
+        skill.mpCost = parseInt(col[5]);
+        skill.tpCost = parseInt(col[6]);
+        skill.scope = parseInt(col[7]);
+        skill.occasion = parseInt(col[8]);
+        skill.speed = parseInt(col[9]);
+        skill.successRate = parseInt(col[10]);
+        skill.repeats = parseInt(col[11]);
+        skill.tpGain = parseInt(col[12]);
+        skill.hitType = parseInt(col[13]);
+        skill.animationId = parseInt(col[14]);
+        skill.message1 = col[15];
+        skill.message2 = col[16];
+        skill.requiredWtypeId1 = parseInt(col[17]);
+        skill.requiredWtypeId2 = parseInt(col[18]);
         skill.damage = {
-            critical: col[2] == 'true' || col[2] == '1' ? true : false, 
-            elementId: parseInt(col[3]), 
-            formula: col[4], 
-            type: parseInt(col[5]), 
-            variance: parseInt(col[6]), 
+            type: parseInt(col[19]), 
+            elementId: parseInt(col[20]), 
+            formula: col[21], 
+            variance: parseInt(col[22]), 
+            critical: col[23] == 'true' || col[23] == '1' ? true : false, 
         };
-        skill.description = col[7];
-        skill.effects = decodeJsonData(col[8]);
-        skill.hitType = parseInt(col[9]);
-        skill.iconIndex = parseInt(col[10]);
-        skill.message1 = col[11];
-        skill.message2 = col[12];
-        skill.mpCost = parseInt(col[13]);
-        skill.name = col[14];
-        skill.note = col[15];
-        skill.occasion = parseInt(col[16]);
-        skill.repeats = parseInt(col[17]);
-        skill.requiredWtypeId1 = parseInt(col[18]);
-        skill.requiredWtypeId2 = parseInt(col[19]);
-        skill.scope = parseInt(col[20]);
-        skill.speed = parseInt(col[21]);
-        skill.stypeId = parseInt(col[22]);
-        skill.successRate = parseInt(col[23]);
-        skill.tpCost = parseInt(col[24]);
-        skill.tpGain = parseInt(col[25]);
+        skill.effects = decodeJsonData(col[24]);
+        skill.note = col[25];
         skill.messageType = parseInt(col[26]);
 
         json.push(skill);
